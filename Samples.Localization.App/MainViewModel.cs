3af467e55dff35ac7e2ad2a5f528ffc85cc53ad2
dmitrybublik <dmitry.buba@gmail.com>
2016-09-21 23:22:31 +0300

    Made Localization sample.

---
+++ Samples.Localization.App/MainViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+using JetBrains.Annotations;
+
+namespace Samples.Localization.App
+{
+    public enum CategoryTypes
+    {
+        None,
+        Load
+    }
+
+    public class Product : INotifyPropertyChanged
+    {
+        private CategoryTypes _category;
+
+        public CategoryTypes Category
+        {
+            get { return _category; }
+            set
+            {
+                if (value == _category)
+                {
+                    return;
+                }
+
+                _category = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Product()
+        {
+            Category = CategoryTypes.Load;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class SessionViewModel
+    {
+        public Product Product { get; set; }
+
+        public SessionViewModel()
+        {
+            Product = new Product();
+        }
+    }
+
+    public sealed class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly Random _rnd = new Random();
+        private readonly DispatcherTimer _timer;
+        
+        public MainViewModel()
+        {
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(500)
+            };
+            _timer.Tick += OnTimer;
+
+            CurrentSessions = new SessionViewModel();
+
+            _timer.Start();
+        }
+
+        private void OnTimer(object sender, EventArgs e)
+        {
+            var categoris = (CategoryTypes[]) Enum.GetValues(typeof(CategoryTypes));
+            var index = _rnd.Next(categoris.Length);
+            CurrentSessions.Product.Category = categoris[index];
+
+            IdentityValueZeroLength = _rnd.NextDouble() >= 0.5;
+        }
+
+        private bool _identityValueZeroLength = false;
+
+        public bool IdentityValueZeroLength
+        {
+            get { return _identityValueZeroLength; }
+            private set
+            {
+                if (_identityValueZeroLength == value)
+                {
+                    return;
+                }
+
+                _identityValueZeroLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SessionViewModel CurrentSessions { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Samples.Localization.App/XPropertyChangedTrigger.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using Microsoft.Expression.Interactivity.Core;
+
+namespace Samples.Localization.App
+{
+    public class XPropertyChangedTrigger : PropertyChangedTrigger
+    {
+        #region RespectLoadedEvent Dependency Property
+
+        public bool RespectLoadedEvent
+        {
+            get { return (bool)GetValue(RespectLoadedEventProperty); }
+            set { SetValue(RespectLoadedEventProperty, value); }
+        }
+
+        public static readonly DependencyProperty RespectLoadedEventProperty =
+            DependencyProperty.Register(
+                "RespectLoadedEvent", 
+                typeof(bool), 
+                typeof(XPropertyChangedTrigger), 
+                new PropertyMetadata(
+                    default(bool), 
+                    OnRespectLoadedEventChanged));
+
+        private static void OnRespectLoadedEventChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        protected override void OnAttached()
+        {
+            if (AssociatedObject is FrameworkElement && RespectLoadedEvent)
+            {
+                ((FrameworkElement)AssociatedObject).Loaded += XDataTriggerLoaded;
+            }
+
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (AssociatedObject is FrameworkElement)
+            {
+                ((FrameworkElement)AssociatedObject).Loaded -= XDataTriggerLoaded;
+            }
+
+        }
+
+        private void XDataTriggerLoaded(object sender, RoutedEventArgs e)
+        {
+            EvaluateBindingChange(e);
+        }
+    }
+
+    public sealed class EmptyMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
